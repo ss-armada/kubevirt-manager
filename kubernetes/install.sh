3f98ba8eb34dd86c147416d107387b5878502930
ss-armada <ss@armada.ai>
2024-10-23 23:10:59 -0700

    added vm-demo related files

---
+++ kubernetes/install.sh
@@ -0,0 +1,6 @@
+kubectl apply -f kubernetes/ns.yaml
+kubectl apply -f kubernetes/crd.yaml
+kubectl apply -f kubernetes/rbac.yaml
+kubectl apply -f kubernetes/deployment.yaml
+kubectl apply -f kubernetes/pc.yaml
+kubectl apply -f kubernetes/service.yaml
